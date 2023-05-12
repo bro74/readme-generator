a1f566b2091e9db441b293b2831cd3274484cd76
Bro74 <bmwhite1014@gmail.com>
2023-05-12 18:15:50 -0400

    arrary of questions for user input

--- index.js
+++ index.js
@@ -7,12 +7,52 @@ const questions = ['What was your motivation?', 'Why did you build this project?
 
 console.log(questions)
 
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'motivation',
+        message: questions[0],
+    },
+    {
+        type: 'input',
+        name: 'build',
+        message: questions[1],
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        message: questions[2],
+    },
+    {
+        type: 'input',
+        name: 'learn',
+        message: questions[3],
+    },
+]).then((answers) => {
+    const fileName = 'README.md';
+    const data = `
+    # ${answers.motivation}
+    
+    ## Description
+    
+    ${answers.build}
+    
+    ## Problem Solved
+    
+    ${answers.problem}
+    
+    ## What I Learned
+    
+    ${answers.learn}
+    `;
+    writeToFile(fileName, data);
+});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
